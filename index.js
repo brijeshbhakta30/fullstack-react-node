f40ccb4a67d27c22eac021de4acc2a3c83e75787
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2019-11-09 17:40:21 +0530

    refactor: renamed project json file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './app';
-import { name as appName } from './project.json';
+import { name as appName } from './application.json';
 
 AppRegistry.registerComponent(appName, () => App);
